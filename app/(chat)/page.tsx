8242969a9e5a0dbba84159c02de41a95fe0de831
kimtaewoong <ktw2172@gmail.com>
2025-06-14 19:24:29 +0900

    fix: Chat 페이지 불필요한 속성 제거

--- app/(chat)/page.tsx
+++ app/(chat)/page.tsx
@@ -27,7 +27,6 @@ export default async function Page() {
         id={id}
         initialMessages={[]}
         initialChatModel={chatModel}
-        initialVisibilityType="private"
         isReadonly={false}
         session={session}
         autoResume={false}
